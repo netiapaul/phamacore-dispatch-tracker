3e42c1ce8c675ad1d0f9499a677e7553b1b3cfff
Netia Paul <netiapaul23@gmail.com>
2025-08-15 10:39:58 +0300

    new Invoice API resource route

--- helpers/url_helper.jsx
+++ helpers/url_helper.jsx
@@ -2,4 +2,5 @@
 export const AUTH_LOGIN_USER = "/Auth";
 
 //INVOICES
-export const INVOICE_LIST = "/Invoices";
+// export const INVOICE_LIST = "/Invoices";
+export const INVOICE_LIST = "/api/v2/invoices";
